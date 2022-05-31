3b7b8e14d48b8a493940647b85fc825728213cc8
Jonathan Lewis <jonathandavidlewis@gmail.com>
2022-05-31 08:29:10 -0700

    Adds pseudocode

---
+++ pseudocode/problem_011.py
@@ -0,0 +1,12 @@
+# Complete the is_divisible_by_5 function to return the
+# word "buzz" if the value in the number parameter is
+# divisible by 5. Otherwise, just return the number.
+
+# def is_divisible_by_5(number):
+    # If number is divisible by 5
+        # return "buzz"
+    # Otherwise
+        # return the number
+
+def is_divisible_by_5(number):
+    pass
