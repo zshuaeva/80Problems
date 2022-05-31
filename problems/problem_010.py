ca9d03bbb1e152215c6f42a548f7e5e8155ab9c1
Jonathan Lewis <jonathandavidlewis@gmail.com>
2022-05-31 07:11:02 -0700

    Add back prob 10

---
+++ problems/problem_010.py
@@ -0,0 +1,9 @@
+# Complete the is_divisible_by_3 function to return the
+# word "fizz" if the value in the number parameter is
+# divisible by 3. Otherwise, just return the number.
+#
+# You can use the test number % 3 == 0 to test if a
+# number is divisible by 3.
+
+def is_divisible_by_3(number):
+    pass
